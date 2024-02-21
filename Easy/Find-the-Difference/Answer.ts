37871d6be61b80485a35938b96257618cf86b049
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-21 22:43:19 +0330

    question 6 is solved

---
+++ Easy/Find-the-Difference/Answer.ts
@@ -0,0 +1,7 @@
+function findTheDifference(s: string, t: string): string {
+    let charCode = t.charCodeAt(0);
+    for (let i = 0; i < s.length; i++) {
+        charCode += t.charCodeAt(i + 1) - s.charCodeAt(i);
+    }
+    return String.fromCharCode(charCode);
+};
