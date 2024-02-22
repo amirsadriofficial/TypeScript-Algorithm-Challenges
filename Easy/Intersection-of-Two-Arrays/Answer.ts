f4fc070a744aecc596eeea8e3c3e1af6a25f7e0e
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-22 23:35:41 +0330

    question 7 is solved

--- Easy/Intersection-of-Two-Arrays/Answer.ts
+++ Easy/Intersection-of-Two-Arrays/Answer.ts
@@ -1,4 +1,4 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    let result = nums1.filter(num => nums2.includes(num));
-    return [...new Set(result)];
-};
+  let result = nums1.filter((num) => nums2.includes(num));
+  return [...new Set(result)];
+}
