687514b9c49d2b64a8270a34f0ca34c97d12164f
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-23 23:47:04 +0330

    question 8 is solved

--- Easy/Intersection-of-Two-Arrays-II/Answer.ts
+++ Easy/Intersection-of-Two-Arrays-II/Answer.ts
@@ -1,12 +1,12 @@
 function intersect(nums1: number[], nums2: number[]): number[] {
-    const counts: {} = {};
-    const result: number[] = [];
-    nums1.forEach(el => counts[el] = ++counts[el] || 1);
-    for (let el of nums2) {
-        if (counts[el]) {
-            result.push(el);
-            counts[el]--;
-        }
+  const counts: {} = {};
+  const result: number[] = [];
+  nums1.forEach((el) => (counts[el] = ++counts[el] || 1));
+  for (let el of nums2) {
+    if (counts[el]) {
+      result.push(el);
+      counts[el]--;
     }
-    return result;
-};
+  }
+  return result;
+}
