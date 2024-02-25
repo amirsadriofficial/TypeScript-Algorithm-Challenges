d31cf11776080aae2f35f6cf2d5ae805dd515e3b
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-25 11:05:24 +0330

    question 10 is solved

--- Easy/Remove-Duplicates-from-Sorted-Array/Answer.ts
+++ Easy/Remove-Duplicates-from-Sorted-Array/Answer.ts
@@ -1,10 +1,10 @@
 function removeDuplicates(nums: number[]): number {
-    let k: number = 1;
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[i - 1]) {
-            nums[k] = nums[i];
-            k++;
-        }
+  let k: number = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
     }
-    return k;
-};
+  }
+  return k;
+}
