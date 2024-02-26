516221242547d1a595dc155120e58509486cea25
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-26 22:18:01 +0330

    question 11 is solved

--- Easy/Excel-Sheet-Column-Title/Answer.ts
+++ Easy/Excel-Sheet-Column-Title/Answer.ts
@@ -1,10 +1,10 @@
 function convertToTitle(columnNumber: number): string {
-    let str: string = '';
-    let num: number;
-    while (columnNumber > 0) {
-        num = (columnNumber - 1) % 26;
-        str = String.fromCharCode(num + 65) + str;
-        columnNumber = Math.floor((columnNumber - num) / 26);
-    }
-    return str;
-};
+  let str: string = "";
+  let num: number;
+  while (columnNumber > 0) {
+    num = (columnNumber - 1) % 26;
+    str = String.fromCharCode(num + 65) + str;
+    columnNumber = Math.floor((columnNumber - num) / 26);
+  }
+  return str;
+}
