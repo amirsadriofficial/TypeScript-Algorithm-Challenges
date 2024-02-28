18e47d2d6313888e72683b054d76dd0fccc81d1b
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-28 16:32:23 +0330

    question 14 is solved

--- Easy/Contains-Duplicate-II/Answer.ts
+++ Easy/Contains-Duplicate-II/Answer.ts
@@ -1,9 +1,9 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-    let set = new Set<number>();
-    for (let i = 0; i < nums.length; i++) {
-        if (set.has(nums[i])) return true;
-        set.add(nums[i]);
-        if (set.size > k) set.delete(nums[i - k]);
-    }
-    return false;
-};
+  let set = new Set<number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+    if (set.size > k) set.delete(nums[i - k]);
+  }
+  return false;
+}
