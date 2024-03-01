7ca13160c2ef0cb994e617d5bd847d50ede215d7
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-01 17:02:16 +0330

    question 16 is solved

---
+++ Easy/Pascal's-Triangle-II/Answer.ts
@@ -0,0 +1,16 @@
+function getRow(rowIndex: number): number[] {
+    if (rowIndex === 0) {
+        return [1];
+    }
+    let triangle: number[][] = [[1]];
+    for (let i = 0; i < rowIndex; i++) {
+        let lastRow: number[] = triangle[triangle.length - 1];
+        let newRow: number[] = [1];
+        for (let j = 1; j < lastRow.length; j++) {
+            newRow.push(lastRow[j - 1] + lastRow[j]);
+        }
+        newRow.push(1);
+        triangle.push(newRow);
+    }
+    return triangle[triangle.length - 1];
+};
---
+++ Easy/Pascal's-Triangle-II/Question.md
@@ -0,0 +1,23 @@
+119. **Pascal's Triangle II**
+
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.<br>
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:<br>
+
+
+ 
+
+Example 1:<br>
+Input: rowIndex = 3<br>
+Output: [1,3,3,1]<br>
+
+Example 2:<br>
+Input: rowIndex = 0<br>
+Output: [1]<br>
+
+Example 3:<br>
+Input: rowIndex = 1<br>
+Output: [1,1]<br>
+
+Constraints:<br>
+0 <= rowIndex <= 33
