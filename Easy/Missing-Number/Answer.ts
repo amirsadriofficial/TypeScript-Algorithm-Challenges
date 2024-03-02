c2b098c969e76eb6cb607ea207dc01cdd5d2e881
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-02 15:50:56 +0330

    question 18 is solved

--- Easy/Missing-Number/Answer.ts
+++ Easy/Missing-Number/Answer.ts
@@ -1,6 +1,6 @@
 function missingNumber(nums: number[]): number {
-    let n = nums.length;
-    let total = (n * (n + 1)) / 2;
-    let sum = nums.reduce((a, b) => a + b);
-    return total - sum;
-};
+  let n = nums.length;
+  let total = (n * (n + 1)) / 2;
+  let sum = nums.reduce((a, b) => a + b);
+  return total - sum;
+}
