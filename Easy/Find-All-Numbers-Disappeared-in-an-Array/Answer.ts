7267cf2413537d2f22c8ec90b4649247b96530a3
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-13 23:13:31 +0330

    Question 23 is solved

---
+++ Easy/Find-All-Numbers-Disappeared-in-an-Array/Answer.ts
@@ -0,0 +1,10 @@
+function findDisappearedNumbers(nums: number[]): number[] {
+    let set = new Set(nums);
+    let res: number[] = [];
+    for (let i = 1; i < nums.length + 1; i++) {
+        if (!set.has(i)) {
+            res.push(i);
+        }
+    }
+    return res;
+};
