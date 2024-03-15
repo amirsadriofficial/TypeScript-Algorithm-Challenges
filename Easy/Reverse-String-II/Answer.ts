43e62c25b73bc71f0be590b3eaf8692c50171063
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-15 21:16:55 +0330

    Question 25 is solved

---
+++ Easy/Reverse-String-II/Answer.ts
@@ -0,0 +1,17 @@
+function reverseStr(s: string, k: number): string {
+    const arr = s.split('');
+    for (let i = 0; i < arr.length; i += 2 * k) {
+        let left = i;
+        let right = i + k - 1;
+        while (left < right) {
+            if (arr[right] === undefined) {
+                right--;
+                continue;
+            }
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    return arr.join('');
+};
