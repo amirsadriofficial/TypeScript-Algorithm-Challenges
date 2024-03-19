2d03c97bf1e2793aa46f15566a69866781713d60
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-19 13:29:48 +0330

    Question 30 is solved

--- Easy/Maximum-Number-of-Words-Found-in-Sentences/Answer.ts
+++ Easy/Maximum-Number-of-Words-Found-in-Sentences/Answer.ts
@@ -1,11 +1,11 @@
 function mostWordsFound(sentences: string[]): number {
-    let max = 0;
-    for(let idx = 0; idx < sentences.length; idx++) {
-        let count = 1;
-        for(const char of sentences[idx]) {
-            if(char === " ") count++;
-        }
-        max = Math.max(count, max);
+  let max = 0;
+  for (let idx = 0; idx < sentences.length; idx++) {
+    let count = 1;
+    for (const char of sentences[idx]) {
+      if (char === " ") count++;
     }
-    return max;
-};
+    max = Math.max(count, max);
+  }
+  return max;
+}
