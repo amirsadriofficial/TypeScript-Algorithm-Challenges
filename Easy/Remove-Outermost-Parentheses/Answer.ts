c8f60ae9cff4dccaa1aa65a27d4b336de003511c
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-19 13:26:25 +0330

    Question 29 is solved

--- Easy/Remove-Outermost-Parentheses/Answer.ts
+++ Easy/Remove-Outermost-Parentheses/Answer.ts
@@ -9,24 +9,29 @@
 //     return built
 // };
 
-function removeOuterParenthesesRec(s: string, index: number, count: number, output: string): string {
-    if (index === s.length) {
-        return output;
+function removeOuterParenthesesRec(
+  s: string,
+  index: number,
+  count: number,
+  output: string
+): string {
+  if (index === s.length) {
+    return output;
+  }
+  const currentChar: string = s.charAt(index);
+  if (currentChar === "(") {
+    count++;
+    if (count > 1) {
+      output += currentChar;
     }
-    const currentChar: string = s.charAt(index);
-    if (currentChar === "(") {
-        count++;
-        if (count > 1) {
-            output += currentChar;
-        }
-    } else {
-        if (count > 1) {
-            output += currentChar;
-        }
-        count--;
+  } else {
+    if (count > 1) {
+      output += currentChar;
     }
-    return removeOuterParenthesesRec(s, index + 1, count, output);
+    count--;
+  }
+  return removeOuterParenthesesRec(s, index + 1, count, output);
 }
 function removeOuterParentheses(s: string): string {
-    return removeOuterParenthesesRec(s, 0, 0, "");
-};
+  return removeOuterParenthesesRec(s, 0, 0, "");
+}
