ae62de45fc531d2e57f873c024cee49b377d2307
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-25 00:07:06 +0330

    Question 34 is solved

--- Medium/Find-the-Duplicate-Number/Answer.ts
+++ Medium/Find-the-Duplicate-Number/Answer.ts
@@ -1,14 +1,14 @@
 function findDuplicate(nums: number[]): number {
-    let slow = 0;
-    let fast = 0;
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow !== fast);
-    let slow2 = 0;
-    while (slow !== slow2) {
-        slow = nums[slow];
-        slow2 = nums[slow2];
-    }
-    return slow;
-};
+  let slow = 0;
+  let fast = 0;
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+  let slow2 = 0;
+  while (slow !== slow2) {
+    slow = nums[slow];
+    slow2 = nums[slow2];
+  }
+  return slow;
+}
