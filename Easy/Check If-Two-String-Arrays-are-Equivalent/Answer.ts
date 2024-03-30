b064583397dbeaa6d4658652f1322b74794193c0
Amir Sadri <amirsadriofficial@gmail.com>
2024-03-30 19:17:52 +0330

    Question 36 is solved

--- Easy/Check If-Two-String-Arrays-are-Equivalent/Answer.ts
+++ Easy/Check If-Two-String-Arrays-are-Equivalent/Answer.ts
@@ -1,8 +1,8 @@
 function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
-    const str1 = word1.join("");
-    const str2 = word2.join("");
-    if (str1 === str2) {
-        return true;
-    }
-    return false;
-};
+  const str1 = word1.join("");
+  const str2 = word2.join("");
+  if (str1 === str2) {
+    return true;
+  }
+  return false;
+}
