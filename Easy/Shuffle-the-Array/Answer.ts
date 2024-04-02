b3a21846aa26df5ee9d928a661dfe8c2f0936935
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-02 10:54:04 +0330

    Question 38 is solved

--- Easy/Shuffle-the-Array/Answer.ts
+++ Easy/Shuffle-the-Array/Answer.ts
@@ -1,8 +1,8 @@
 function shuffle(nums: number[], n: number): number[] {
-    let result: number[] = [];
-    for (let i = 0; i < n; i++) {
-        result.push(nums[i]);
-        result.push(nums[n + i]);
-    }
-    return result;
-};
+  let result: number[] = [];
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i]);
+    result.push(nums[n + i]);
+  }
+  return result;
+}
