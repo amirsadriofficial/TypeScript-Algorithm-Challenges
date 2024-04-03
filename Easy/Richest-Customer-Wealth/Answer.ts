1fb36a20d50faca743f4f35848ccdcf643f0339c
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-03 16:17:46 +0330

    Question 39 is solved

---
+++ Easy/Richest-Customer-Wealth/Answer.ts
@@ -0,0 +1,8 @@
+function maximumWealth(accounts: number[][]): number {
+    let maxWealth: number = 0;
+    for (let account of accounts) {
+        const sum = account.reduce((runningTotal, nextValue) => runningTotal + nextValue, 0);
+        maxWealth = Math.max(maxWealth, sum);
+    }
+    return maxWealth;
+};
