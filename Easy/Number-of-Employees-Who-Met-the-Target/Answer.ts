819547a2bffbe8a20ca247eaf6ec913f5529e3f6
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-04 19:13:33 +0330

    Question 40 is solved

--- Easy/Number-of-Employees-Who-Met-the-Target/Answer.ts
+++ Easy/Number-of-Employees-Who-Met-the-Target/Answer.ts
@@ -1,7 +1,10 @@
-function numberOfEmployeesWhoMetTarget(hours: number[], target: number): number {
-    let count: number = 0;
-    for (let i = 0; i < hours.length; i++) {
-        if (hours[i] >= target) count++;
-    }
-    return count;
-};
+function numberOfEmployeesWhoMetTarget(
+  hours: number[],
+  target: number
+): number {
+  let count: number = 0;
+  for (let i = 0; i < hours.length; i++) {
+    if (hours[i] >= target) count++;
+  }
+  return count;
+}
