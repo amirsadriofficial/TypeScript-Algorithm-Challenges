5c7ab48619135b89db7d1522b5d2bd4741e1b689
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-08 12:33:24 +0330

    Question 44 is solved

--- Medium/Add-Two-Numbers/Answer.ts
+++ Medium/Add-Two-Numbers/Answer.ts
@@ -1,16 +1,19 @@
 //  Definition for singly-linked list.
 class ListNodeClass {
-    val: number
-    next: any;
-    constructor(val?: number, next?: any) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
+  val: number;
+  next: any;
+  constructor(val?: number, next?: any) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 
 function addTwoNumbers(l1: any, l2: any, carry: number = 0): any {
-    if (!l1 && !l2 && !carry) return null;
-    var total: number = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0);
-    carry = parseInt(total / 10 + '');
-    return new ListNodeClass(total % 10, addTwoNumbers(l1?.next, l2?.next, carry));
-};
+  if (!l1 && !l2 && !carry) return null;
+  var total: number = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0);
+  carry = parseInt(total / 10 + "");
+  return new ListNodeClass(
+    total % 10,
+    addTwoNumbers(l1?.next, l2?.next, carry)
+  );
+}
