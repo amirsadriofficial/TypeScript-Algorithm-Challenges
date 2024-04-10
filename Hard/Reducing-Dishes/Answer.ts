1c67409849ff1c220a5ccdc4fbda1ed2ad5e16b9
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-10 17:20:56 +0330

    Question 45 is solved

--- Hard/Reducing-Dishes/Answer.ts
+++ Hard/Reducing-Dishes/Answer.ts
@@ -1,14 +1,14 @@
 function maxSatisfaction(satisfaction: number[]): number {
-    let sorted = satisfaction.sort((a, b) => a - b);
-    let total = sorted.reduce((a, b) => a + b);
-    let remove = 0;
-    while (total < 0) {
-        total -= sorted[remove];
-        remove++;
-    }
-    let result = 0;
-    for (let i = remove; i < sorted.length; i++) {
-        result += sorted[i] * (i - remove + 1);
-    }
-    return result;
-};
+  let sorted = satisfaction.sort((a, b) => a - b);
+  let total = sorted.reduce((a, b) => a + b);
+  let remove = 0;
+  while (total < 0) {
+    total -= sorted[remove];
+    remove++;
+  }
+  let result = 0;
+  for (let i = remove; i < sorted.length; i++) {
+    result += sorted[i] * (i - remove + 1);
+  }
+  return result;
+}
