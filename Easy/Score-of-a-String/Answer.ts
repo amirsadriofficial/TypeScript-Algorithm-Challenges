b54da8d83522268d096be9db1bb1955d21f00007
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-18 15:55:57 +0330

    Question 49 is solved

--- Easy/Score-of-a-String/Answer.ts
+++ Easy/Score-of-a-String/Answer.ts
@@ -1,10 +1,10 @@
 function scoreOfString(s: string): number {
-    let ans: number = 0;
-    if (s) {
-        let sArray: string[] = s.split("");
-        for (let i = 0; i < sArray.length - 1; i++) {
-            ans += Math.abs(sArray[i].charCodeAt(0) - sArray[i + 1].charCodeAt(0));
-        }
+  let ans: number = 0;
+  if (s) {
+    let sArray: string[] = s.split("");
+    for (let i = 0; i < sArray.length - 1; i++) {
+      ans += Math.abs(sArray[i].charCodeAt(0) - sArray[i + 1].charCodeAt(0));
     }
-    return ans;
-};
+  }
+  return ans;
+}
