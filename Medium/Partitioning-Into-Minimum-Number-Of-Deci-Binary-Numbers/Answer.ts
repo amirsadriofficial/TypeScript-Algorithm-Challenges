a629895d146c14f6da17ac581a0034616abea46c
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-19 13:37:08 +0330

    Question 50 is solved

--- Medium/Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers/Answer.ts
+++ Medium/Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers/Answer.ts
@@ -1,5 +1,5 @@
 function minPartitions(n: string): number | undefined {
-    for (let i = 9; i > 0; i--) {
-        if (n.indexOf(i.toString()) != -1) return i
-    }
-};
+  for (let i = 9; i > 0; i--) {
+    if (n.indexOf(i.toString()) != -1) return i;
+  }
+}
