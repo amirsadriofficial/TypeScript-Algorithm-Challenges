8073d67a3e3d6212f66ec7aba8652c1b713a8933
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-20 11:03:31 +0330

    Question 51 is solved

--- Easy/Jewels-and-Stones/Answer.ts
+++ Easy/Jewels-and-Stones/Answer.ts
@@ -1,9 +1,9 @@
 function numJewelsInStones(jewels: string, stones: string): number {
-    let jewelsCounter = 0;
-    for (let i = 0; i < stones.length; i++) {
-        if (jewels.indexOf(stones[i]) >= 0) {
-            jewelsCounter++;
-        }
+  let jewelsCounter = 0;
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.indexOf(stones[i]) >= 0) {
+      jewelsCounter++;
     }
-    return jewelsCounter;
-};
+  }
+  return jewelsCounter;
+}
