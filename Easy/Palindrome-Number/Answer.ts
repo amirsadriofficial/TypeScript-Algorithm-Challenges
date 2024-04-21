d09576fae67059e05a114afbcc208f0fee0fb1aa
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-21 12:20:48 +0330

    Question 52 is solved

--- Easy/Palindrome-Number/Answer.ts
+++ Easy/Palindrome-Number/Answer.ts
@@ -1,9 +1,8 @@
 function isPalindrome(x: number): boolean {
-    var str = (x.toString()).split('').reverse().join('');
-    if (x === parseInt(str)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+  var str = x.toString().split("").reverse().join("");
+  if (x === parseInt(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
