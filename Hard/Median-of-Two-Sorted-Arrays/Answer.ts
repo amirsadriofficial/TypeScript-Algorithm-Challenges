1a796353a33a2312c029191e225862db46231b9f
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-24 14:04:46 +0330

    Question 55 is solved

--- Hard/Median-of-Two-Sorted-Arrays/Answer.ts
+++ Hard/Median-of-Two-Sorted-Arrays/Answer.ts
@@ -1,23 +1,26 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    const totalLength = nums1.length + nums2.length;
-    const isEven = totalLength % 2 === 0;
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let median1 = 0;
-    let median2 = 0;
-    const nums1Length = nums1.length
-    const nums2Length = nums2.length
-    for (let i = 0; i <= totalLength / 2; i++) {
-        median1 = median2;
-        let nums1LeftIndex = nums1[leftIndex]
-        let nums2LeftIndex = nums2[rightIndex]
-        if (leftIndex < nums1Length && (rightIndex >= nums2Length || nums1LeftIndex < nums2LeftIndex)) {
-            median2 = nums1LeftIndex;
-            leftIndex++;
-        } else {
-            median2 = nums2LeftIndex;
-            rightIndex++;
-        }
+  const totalLength = nums1.length + nums2.length;
+  const isEven = totalLength % 2 === 0;
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let median1 = 0;
+  let median2 = 0;
+  const nums1Length = nums1.length;
+  const nums2Length = nums2.length;
+  for (let i = 0; i <= totalLength / 2; i++) {
+    median1 = median2;
+    let nums1LeftIndex = nums1[leftIndex];
+    let nums2LeftIndex = nums2[rightIndex];
+    if (
+      leftIndex < nums1Length &&
+      (rightIndex >= nums2Length || nums1LeftIndex < nums2LeftIndex)
+    ) {
+      median2 = nums1LeftIndex;
+      leftIndex++;
+    } else {
+      median2 = nums2LeftIndex;
+      rightIndex++;
     }
-    return isEven ? (median1 + median2) / 2 : median2;
-};
+  }
+  return isEven ? (median1 + median2) / 2 : median2;
+}
