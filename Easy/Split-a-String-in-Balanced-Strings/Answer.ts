ffb0f2d2e8bae13b32a0fc9977998d46a457c2dd
Amir Sadri <amirsadriofficial@gmail.com>
2024-04-30 11:16:08 +0330

    Question 59 is solved

--- Easy/Split-a-String-in-Balanced-Strings/Answer.ts
+++ Easy/Split-a-String-in-Balanced-Strings/Answer.ts
@@ -1,10 +1,10 @@
 function balancedStringSplit(s: string): number {
-    let ans = 0;
-    const chars = { L: -1, R: 1 };
-    let cur = 0;
-    for (let right = 0; right < s.length; right++) {
-        cur += chars[s[right]];
-        if (cur === 0) ans++;
-    }
-    return ans;
-};
+  let ans = 0;
+  const chars = { L: -1, R: 1 };
+  let cur = 0;
+  for (let right = 0; right < s.length; right++) {
+    cur += chars[s[right]];
+    if (cur === 0) ans++;
+  }
+  return ans;
+}
