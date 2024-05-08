db5f7dc0706eb068c0f3d03145f0913eb4a87868
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-08 18:52:37 +0330

    Question 62 is solved

---
+++ Medium/Permutations/Answer.ts
@@ -0,0 +1,18 @@
+function permute(nums: number[]): number[][] {
+    const answer: any = [];
+    function backtrack(current: number[], num: number) {
+        if (current.includes(num)) return;
+        current.push(num);
+        if (current.length === nums.length) {
+            answer.push(current);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            backtrack([...current], nums[i]);
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        backtrack([], nums[i]);
+    }
+    return answer;
+};
---
+++ Medium/Permutations/Question.md
@@ -0,0 +1,22 @@
+46. **Permutations**
+
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.<br>
+
+ 
+
+Example 1:<br>
+Input: nums = [1,2,3]<br>
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]<br>
+
+Example 2:<br>
+Input: nums = [0,1]<br>
+Output: [[0,1],[1,0]]<br>
+
+Example 3:<br>
+Input: nums = [1]<br>
+Output: [[1]]<br>
+
+Constraints:<br>
+1 <= nums.length <= 6<br>
+-10 <= nums[i] <= 10<br>
+All the integers of nums are unique.
