8af227dfd70c7a647f961cf4a746ff8ef3a674ef
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-12 17:37:27 +0330

    Question 64 is solved

--- Medium/3Sum/Answer.ts
+++ Medium/3Sum/Answer.ts
@@ -1,23 +1,22 @@
 function threeSum(nums: number[]): number[][] {
-    nums.sort((a, b) => a - b);
-    const res: number[][] = [];
-    nums.forEach((value, idx): void => {
-      if (idx > 0 && value === nums[idx - 1]) return;
-      let left = idx + 1
-      let right = nums.length - 1;
-      while (left < right) {
-        let sum = value + nums[left] + nums[right];
-        if (sum > 0) {
-          right--;
-        } else if (sum < 0) {
-          left++;
-        } else {
-          res.push([value, nums[left], nums[right]]);
-          left++;
-          while (left < right && nums[left] === nums[left - 1]) left++;
-        }
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+  nums.forEach((value, idx): void => {
+    if (idx > 0 && value === nums[idx - 1]) return;
+    let left = idx + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = value + nums[left] + nums[right];
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        res.push([value, nums[left], nums[right]]);
+        left++;
+        while (left < right && nums[left] === nums[left - 1]) left++;
       }
-    });
-    return res;
-  }
-  
+    }
+  });
+  return res;
+}
