1dd41e3d361a2d5edb911bc10c5fb42074ef86ff
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-13 16:05:31 +0330

    Question 65 is solved

--- Medium/Generate-Parentheses/Answer.ts
+++ Medium/Generate-Parentheses/Answer.ts
@@ -1,19 +1,17 @@
 function generateParenthesis(n: number): string[] {
-    const allCombos: string[] = []
-    function recurse(
-        openParens: number, closedParens: number, combo: string
-    ) {
-        if (openParens === 0 && closedParens === 0) {
-            allCombos.push(combo)
-            return
-        }
-        if (openParens < closedParens) {
-            recurse(openParens, closedParens - 1, combo + ')');
-        }
-        if (openParens > 0) {
-            recurse(openParens - 1, closedParens, combo + '(');
-        }
+  const allCombos: string[] = [];
+  function recurse(openParens: number, closedParens: number, combo: string) {
+    if (openParens === 0 && closedParens === 0) {
+      allCombos.push(combo);
+      return;
     }
-    recurse(n, n, '');
-    return allCombos;
-};
+    if (openParens < closedParens) {
+      recurse(openParens, closedParens - 1, combo + ")");
+    }
+    if (openParens > 0) {
+      recurse(openParens - 1, closedParens, combo + "(");
+    }
+  }
+  recurse(n, n, "");
+  return allCombos;
+}
