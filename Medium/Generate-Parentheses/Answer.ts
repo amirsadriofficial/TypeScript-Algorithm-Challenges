6d7019102f88b343ec7e61ac616b758896029668
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-13 16:05:04 +0330

    Question 65 is solved

---
+++ Medium/Generate-Parentheses/Answer.ts
@@ -0,0 +1,19 @@
+function generateParenthesis(n: number): string[] {
+    const allCombos: string[] = []
+    function recurse(
+        openParens: number, closedParens: number, combo: string
+    ) {
+        if (openParens === 0 && closedParens === 0) {
+            allCombos.push(combo)
+            return
+        }
+        if (openParens < closedParens) {
+            recurse(openParens, closedParens - 1, combo + ')');
+        }
+        if (openParens > 0) {
+            recurse(openParens - 1, closedParens, combo + '(');
+        }
+    }
+    recurse(n, n, '');
+    return allCombos;
+};
---
+++ Medium/Generate-Parentheses/Question.md
@@ -0,0 +1,16 @@
+22. **Generate Parentheses**
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.<br>
+
+ 
+
+Example 1:<br>
+Input: n = 3<br>
+Output: ["((()))","(()())","(())()","()(())","()()()"]<br>
+
+Example 2:<br>
+Input: n = 1<br>
+Output: ["()"]<br>
+
+Constraints:<br>
+1 <= n <= 8
