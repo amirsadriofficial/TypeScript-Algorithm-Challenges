a223a97c24cb63fbad9ea35b3522f195a9396e4f
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-16 13:44:44 +0330

    Question 66 is solved

--- Medium/Longest-Substring-Without-Repeating-Characters/Answer.ts
+++ Medium/Longest-Substring-Without-Repeating-Characters/Answer.ts
@@ -1,11 +1,13 @@
 function lengthOfLongestSubstring(s: string): number {
-    const scanner: string[] = [];
-    let longest = 0;
-    for (const char of s) {
-        const possibleIndex = scanner.indexOf(char);
-        if (possibleIndex !== -1) { scanner.splice(0, possibleIndex + 1) };
-        scanner.push(char);
-        longest = Math.max(longest, scanner.length);
+  const scanner: string[] = [];
+  let longest = 0;
+  for (const char of s) {
+    const possibleIndex = scanner.indexOf(char);
+    if (possibleIndex !== -1) {
+      scanner.splice(0, possibleIndex + 1);
     }
-    return longest;
-};
+    scanner.push(char);
+    longest = Math.max(longest, scanner.length);
+  }
+  return longest;
+}
