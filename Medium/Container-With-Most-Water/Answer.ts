0049fce68fe9be13530775981777f51313174609
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-18 11:28:40 +0330

    Question 68 is solved

--- Medium/Container-With-Most-Water/Answer.ts
+++ Medium/Container-With-Most-Water/Answer.ts
@@ -1,14 +1,14 @@
 function maxArea(height: number[]): number {
-    let low = 0;
-    let high = height.length - 1;
-    let ans = 0;
-    while (low <= high) {
-        const length = Math.min(height[low], height[high]);
-        const breadth = high - low;
-        const area = length * breadth;
-        ans = Math.max(ans, area);
-        if (height[low] < height[high]) low++;
-        else high--;
-    }
-    return ans;
-};
+  let low = 0;
+  let high = height.length - 1;
+  let ans = 0;
+  while (low <= high) {
+    const length = Math.min(height[low], height[high]);
+    const breadth = high - low;
+    const area = length * breadth;
+    ans = Math.max(ans, area);
+    if (height[low] < height[high]) low++;
+    else high--;
+  }
+  return ans;
+}
