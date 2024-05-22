fd7622d59834daa69363ad23270247fffc6cd2fb
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-22 22:33:44 +0330

    Question 70 is solved

---
+++ Easy/Longest-Common-Prefix/Answer.ts
@@ -0,0 +1,14 @@
+function longestCommonPrefix(strs: string[]): string {
+    let sorted = strs.sort((a,b) => a < b? -1:1)
+    let output: any = []
+    let firstword = sorted[0]
+    let lastword = sorted[sorted.length -1]
+    for (var i = 0; i < firstword.length; i++) {
+        if (firstword[i] ==lastword[i] ){
+            output.push(firstword[i])
+        }else{       
+            break
+        }
+   }
+    return output.join("")
+};
