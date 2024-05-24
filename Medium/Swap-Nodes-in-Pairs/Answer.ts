d54b5c11bac6cb840da05e62f70ee5e5f8bcce0f
Amir Sadri <amirsadriofficial@gmail.com>
2024-05-24 14:48:20 +0330

    Question 71 is solved

---
+++ Medium/Swap-Nodes-in-Pairs/Answer.ts
@@ -0,0 +1,20 @@
+
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function swapPairs(head: ListNode | null): ListNode | null {
+    if(!head?.next) return head
+    let firstNode = head
+    let secondNode = head.next
+    let remaining = swapPairs(secondNode.next)
+    firstNode.next = remaining
+    secondNode.next = firstNode
+    return secondNode
+};
---
+++ Medium/Swap-Nodes-in-Pairs/Question.md
@@ -0,0 +1,20 @@
+24. **Swap Nodes in Pairs**
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)<br>
+ 
+
+Example 1:<br>
+Input: head = [1,2,3,4]<br>
+Output: [2,1,4,3]<br>
+
+Example 2:<br>
+Input: head = []<br>
+Output: []<br>
+
+Example 3:<br>
+Input: head = [1]<br>
+Output: [1]<br>
+
+Constraints:<br>
+The number of nodes in the list is in the range [0, 100].<br>
+0 <= Node.val <= 100
