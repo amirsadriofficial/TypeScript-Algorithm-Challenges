2723e7fff2dd61b902a56f449ce289426cab882e
Amir Sadri <amirsadriofficial@gmail.com>
2024-06-30 10:59:08 +0330

    Question 75 is solved

---
+++ Medium/Remove-Nth-Node-From-End-of-List/Answer.ts
@@ -0,0 +1,34 @@
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    function findLength(head: ListNode | null): number {
+      let count = 0;
+      if (head === null) return count;
+      let curr: any = head;
+      while (curr !== null) {
+        count++;
+        curr = curr.next;
+      }
+      return count;
+    }
+  let length = findLength(head);
+  let i = 0,
+    traverseTill = length - n - 1;
+  if (traverseTill === -1 && head !== null) return head.next;
+  let curr: any = head;
+  while (i < traverseTill) {
+    curr = curr.next;
+    i++;
+  }
+  curr.next = curr.next.next;
+  return head;
+
+}
---
+++ Medium/Remove-Nth-Node-From-End-of-List/Question.md
@@ -0,0 +1,23 @@
+19. **Remove Nth Node From End of List**
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.<br>
+
+ 
+
+Example 1:<br>
+Input: head = [1,2,3,4,5], n = 2<br>
+Output: [1,2,3,5]<br>
+
+Example 2:<br>
+Input: head = [1], n = 1<br>
+Output: []<br>
+
+Example 3:<br>
+Input: head = [1,2], n = 1<br>
+Output: [1]<br>
+
+Constraints:<br>
+The number of nodes in the list is sz.<br>
+1 <= sz <= 30<br>
+0 <= Node.val <= 100<br>
+1 <= n <= sz
