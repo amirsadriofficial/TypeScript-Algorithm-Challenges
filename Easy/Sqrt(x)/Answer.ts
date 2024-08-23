2ad70544ed6360c2efe7710219127da6e3ef3381
Amir Sadri <amirsadriofficial@gmail.com>
2024-08-23 17:58:19 +0330

    Question 78 is solved

--- Easy/Sqrt(x)/Answer.ts
+++ Easy/Sqrt(x)/Answer.ts
@@ -1,11 +1,11 @@
 function mySqrt(x: number): number {
-    let low = 0;
-    let high = x;
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-        const square = mid * mid;
-        if (square <= x) low = mid + 1;
-        else high = mid - 1;
-    }
-    return high;
+  let low = 0;
+  let high = x;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const square = mid * mid;
+    if (square <= x) low = mid + 1;
+    else high = mid - 1;
+  }
+  return high;
 }
