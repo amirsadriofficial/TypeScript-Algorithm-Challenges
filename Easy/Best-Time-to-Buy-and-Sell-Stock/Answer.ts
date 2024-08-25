51b980826cb94a1842825de49793354edaf94839
Amir Sadri <amirsadriofficial@gmail.com>
2024-08-25 14:00:14 +0330

    Question 79 is solved

--- Easy/Best-Time-to-Buy-and-Sell-Stock/Answer.ts
+++ Easy/Best-Time-to-Buy-and-Sell-Stock/Answer.ts
@@ -1,13 +1,13 @@
 function maxProfit(prices) {
-    let maxProfit = 0;
-    let minPrice = Number.MAX_VALUE;
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else if (prices[i] - minPrice > maxProfit) {
-            maxProfit = prices[i] - minPrice;
-        }
+  let maxProfit = 0;
+  let minPrice = Number.MAX_VALUE;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
     }
+  }
 
-    return maxProfit;
+  return maxProfit;
 }
